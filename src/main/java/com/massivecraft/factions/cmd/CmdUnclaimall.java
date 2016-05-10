ed5b168031b4c6012ec7bc15bf7b4f528cb26efa
Daniel Saukel <sa.da@unitybox.de>
2016-05-10 14:46:11 +0200

    Make LandUnclaimAllEvent cancellable; resolves #6

--- src/main/java/com/massivecraft/factions/cmd/CmdUnclaimall.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdUnclaimall.java
@@ -42,11 +42,13 @@ public class CmdUnclaimall extends FCommand {
         Bukkit.getServer().getPluginManager().callEvent(unclaimAllEvent);
         // this event cannot be cancelled
 
-        Board.unclaimAll(myFaction.getId());
-        myFaction.msg("%s<i> unclaimed ALL of your faction's land.", fme.describeTo(myFaction, true));
+        if (!unclaimAllEvent.isCancelled()) {
+            Board.unclaimAll(myFaction.getId());
+            myFaction.msg("%s<i> unclaimed ALL of your faction's land.", fme.describeTo(myFaction, true));
 
-        if (Conf.logLandUnclaims) {
-            P.p.log(fme.getName() + " unclaimed everything for the faction: " + myFaction.getTag());
+            if (Conf.logLandUnclaims) {
+                P.p.log(fme.getName() + " unclaimed everything for the faction: " + myFaction.getTag());
+            }
         }
     }
 
--- src/main/java/com/massivecraft/factions/event/LandUnclaimAllEvent.java
+++ src/main/java/com/massivecraft/factions/event/LandUnclaimAllEvent.java
@@ -3,12 +3,14 @@ package com.massivecraft.factions.event;
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.Faction;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class LandUnclaimAllEvent extends Event {
+public class LandUnclaimAllEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
 
     private Faction faction;
     private FPlayer fplayer;
@@ -45,6 +47,16 @@ public class LandUnclaimAllEvent extends Event {
 
     public Player getPlayer() {
         return fplayer.getPlayer();
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 
 }
