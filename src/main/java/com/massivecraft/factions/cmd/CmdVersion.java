6e0f4eb0e32924585ddd780f7f9ebed24d73ca0b
Marshall <pureplugins@gmail.com>
2016-12-20 19:23:34 -0800

    removed patch.java bullshit

--- src/main/java/com/massivecraft/factions/cmd/CmdVersion.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdVersion.java
@@ -1,7 +1,6 @@
 package com.massivecraft.factions.cmd;
 
 import com.massivecraft.factions.P;
-import com.massivecraft.factions.Patch;
 import com.massivecraft.factions.struct.Permission;
 
 public class CmdVersion extends FCommand {
@@ -9,9 +8,8 @@ public class CmdVersion extends FCommand {
     public CmdVersion() {
         aliases.add("version");
 
-        // this.requiredArgs.add("");
-        // this.optionalArgs.put("", "");
         permission = Permission.VERSION.node;
+
         disableOnLock = false;
 
         senderMustBePlayer = false;
@@ -22,7 +20,6 @@ public class CmdVersion extends FCommand {
 
     @Override
     public void perform() {
-        msg("<i>You are running " + P.p.getDescription().getFullName() + "\nPatch: " + Patch.getFullName());
+        msg("<i>You are running " + P.p.getDescription().getFullName());
     }
-
 }
